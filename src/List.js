73eb7c98adf8b2cdb1b4e27ae7476f990046cab9
mustafa <mltm0204@gmail.com>
2021-12-17 16:28:22 +0300

    fourth commit

--- src/List.js
+++ src/List.js
@@ -7,7 +7,12 @@ const List = ({people}) => {
         const {id, name, age,image}=person;
         return(
           <article key={id} className='person' >
-            
+          <img src={image} alt={name} />
+          <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+          </div>
+        </article>
         )
 
       })}
