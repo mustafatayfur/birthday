4eb4cc5c8575dfc6aa5695a3a58a44e31d951469
mustafa <mltm0204@gmail.com>
2021-12-17 16:27:39 +0300

    third commit

--- src/List.js
+++ src/List.js
@@ -1,1 +1,18 @@
+import React from 'react';
 
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person)=>{
+        const {id, name, age,image}=person;
+        return(
+          <article key={id} className='person' >
+            
+        )
+
+      })}
+    </>
+  );
+};
+
+export default List;
