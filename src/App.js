de08b98df24df05966a9ba4e7762bf0c223bc444
mustafa <mltm0204@gmail.com>
2021-12-17 16:34:35 +0300

    project is completed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <main>
       <section className='container'>
-        <h3>{people.lenght} birthdays today </h3>
+        <h3>{people.length} birthdays today </h3>
         <List people={people} />
-        <button onClick={()=> console.log('You clicked me')}>clear all</button>
+        <button onClick={()=> setPeople([])}>clear all</button>
       </section>
     </main>
   );
